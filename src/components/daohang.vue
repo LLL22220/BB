b9441a6f1326ac27c62a408f7a60e2cfa8827ae4
LPF <769053624@qq.com>
2024-07-31 17:40:53 +0800

    7.31下班提交

--- src/components/daohang.vue
+++ src/components/daohang.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="banner-Container">
-      <div class="vidContainer">
+  <div class="root">
+    <div class="vidContainer">
         <video class="vid" src="../vedio/headbanner.webm"></video>
         <!-- 导航部分 -->
         <div class="navContainer">
@@ -55,111 +55,92 @@
         <div class="biliLogo">
           <img src="../img/c8fd97a40bf79f03e7b76cbc87236f612caef7b2.png" width="168px" alt="" />
         </div>
-        
-
-      </div>
+    </div>
+    
     <div class="menu-Container">
-        <div class="tubiao-Container">
-        <div class="tubiao" alt="">
+        <div class="m-1">
+          <div class="tubiao" alt="">
             <img src="../img/book.jpg" height="50px" >
             <p>动态</p>
-        </div> 
-        <div class="tubiao" alt="">
+          </div> 
+          <div class="tubiao" alt="">
             <img src="../img/fire.jpg" height="50px" >
             <p>热门</p>
+          </div> 
         </div>
-        <span class="line"></span>
-    <div class="l-2">
-        <a href=""><span>动画</span></a>
-        <a href=""><span>音乐</span></a>
-        <a href=""><span>舞蹈</span></a>
-   <a href="">
-  <span>
-    知识
-   
-  </span>
-</a>
-<a href="">
-  <span>
-    生活
- 
-  </span>
-</a>
-<a href="">
-  <span>
-    时尚
-  
-  </span>
-</a>
-<a href="">
-  <span>
-    娱乐
-  </span>
-</a>
-<a>
-  <span>
-    放映厅
-   
-  </span>
-</a>
-<a href="">
-  <span>
-    番剧
-    
-  </span>
-</a>
-<a href="">
-  <span>
-    国创
-    
-  </span>
-</a>
-<a href="">
-  <span>
-    游戏
-  
-  </span>
-</a>
-<a href="">
-  <span>
-    科技
-    
-  </span>
-</a>
-<a href="">
-  <span>
-    鬼畜
-  
-  </span>
-</a>
-<a href="">
-  <span>
-    资讯
-   
-  </span>
-</a>
-<a href="">
-  <span>
-    影视
-    
-  </span>
-</a>
-<a href="">
-  <span>
-    更多
-    <i class="fa fa-angle-down" style="font-size: 16px"></i>
-  </span>
-</a>
-    div>
+        <div class="m-2">
+            <span>番剧</span>
+            <span>国创</span>
+            <span>综艺</span>
+            <span>动画</span>
+            <span>鬼畜</span>
+            <span>舞蹈</span>
+            <span>娱乐</span>
+            <span>科技</span>
+            <span>美食</span>
+            <span>汽车</span>
+            <span>运动</span>
+            <span>电影</span>
+            <span>电视剧</span>
+            <span>纪录片</span>
+            <span>游戏</span>
+            <span>音乐</span>
+            <span>影视</span>
+            <span>知识</span>
+            <span>资讯</span>
+            <span>生活</span>
+            <span>时尚</span>
+            <span>更多</span>
+      </div>
+      <div class="m-3">
+       
+        <span><img src="../img/book.jpg">专栏</span>
+        <span><img src="../img/notes.png">活动</span>
+        <span><img src="../img/videocamera.png">直播</span>
+        <span><img src="../img/mark.png">课堂</span>
+        <span><img src="../img/building-three.png" >社区中心</span>
+        <span><img src="../img/music-cd.png">新歌热榜</span>
+     
+      </div>
     </div>
-        </div>
-    </div>
-</div>
-
+    <!-- 第二部分 -->
+    <div class="second-section">
+      <!-- 轮播部分 -->
+       <div class="autoshow">
+      <div class="show">
+          <div class="item"><img src="../img/v008endr.png" /></div>
+          <div class="item"><img src="../img/QQ图片20240730115129.jpg" /></div>
+          <div class="item"><img src="../img/v008endr.png" /></div>
+          <div class="item"><img src="../img/v008endr.png" /></div>
+          <div class="item"><img src="../img/v008endr.png" /></div>  
+      </div>    
+      <div class="button">
+        <span class="active"></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div> 
     
-
+    </div>
+    
+      
+   <button @click="run()">检测</button>
+  </div>
 </template>
 <script>
+let left = 0;
+let timer;
+function run(){
+a = document.getElementsByClassName('show')
+a.style.marginLeft + 'px';
+left -= 10;
+timer = setTimeout(run,10);
+}
+
+
+
 import "@/css/daohang.css"
 export default{
 data(){
@@ -168,6 +149,14 @@ data(){
     }
 },
 methods:{
+move(){
+// let a = document.getElementById('show')
+// console.log(a);
+// let b = a.style.transform
+// b = 'translateX(-100%)'
+// console.log(b);
+
+}
    
 }
 }
