774fadb8a5aa06da63d3cc385d3770415ecb5dd3
LPF <769053624@qq.com>
2024-07-31 09:51:41 +0800

    第一次提交BB

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+ <div class="daohang">
+   <daohang />
+
+ </div>
+</div>
+</template>
+
+<script>
+import daohang from './components/daohang.vue'
+
+export default{
+   components:{
+    daohang
+   }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/daohang.vue
@@ -0,0 +1,175 @@
+<template>
+    <div class="banner-Container">
+      <div class="vidContainer">
+        <video class="vid" src="../vedio/headbanner.webm"></video>
+        <!-- 导航部分 -->
+        <div class="navContainer">
+
+          <div class="first-nav">
+            <ul class="f-n">
+              <li>首页</li>
+              <li>番剧</li>
+              <li>直播</li>
+              <li>游戏中心</li>
+              <li>会员购</li>
+              <li>漫画</li>
+              <li>赛事</li>
+              <li>去巴黎</li>
+              <li style="font-size: 16px">下载客户端</li>
+            </ul>
+          </div>
+
+          <!-- 搜索框 -->
+          <div class="searchInput">
+            <input type="text"  placeholder="搜索" />
+            <button>搜索</button>
+          </div>
+          <!-- 用户栏 -->
+          <div class="userSection">
+            <div class="user-left">
+              <!-- 头像 -->
+              <div class="carmer">
+                <img
+                  src="../img/QQ图片20240730115129.jpg"
+                  width="36px"
+                  height="36px"
+                  alt=""
+                />
+              </div>
+              <a href="">大会员</a>
+              <a href="">消息</a>
+              <a href="">动态</a>
+              <a href="">大会员</a>
+              <a href="">历史</a>
+              <a href="">创作中心</a>
+            </div>
+
+            <div class="user-right">
+              <div class="userDetail">
+                <a href="" style="color: white">投稿</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="biliLogo">
+          <img src="../img/c8fd97a40bf79f03e7b76cbc87236f612caef7b2.png" width="168px" alt="" />
+        </div>
+        
+
+      </div>
+    <div class="menu-Container">
+        <div class="tubiao-Container">
+        <div class="tubiao" alt="">
+            <img src="../img/book.jpg" height="50px" >
+            <p>动态</p>
+        </div> 
+        <div class="tubiao" alt="">
+            <img src="../img/fire.jpg" height="50px" >
+            <p>热门</p>
+        </div>
+        <span class="line"></span>
+    <div class="l-2">
+        <a href=""><span>动画</span></a>
+        <a href=""><span>音乐</span></a>
+        <a href=""><span>舞蹈</span></a>
+   <a href="">
+  <span>
+    知识
+   
+  </span>
+</a>
+<a href="">
+  <span>
+    生活
+ 
+  </span>
+</a>
+<a href="">
+  <span>
+    时尚
+  
+  </span>
+</a>
+<a href="">
+  <span>
+    娱乐
+  </span>
+</a>
+<a>
+  <span>
+    放映厅
+   
+  </span>
+</a>
+<a href="">
+  <span>
+    番剧
+    
+  </span>
+</a>
+<a href="">
+  <span>
+    国创
+    
+  </span>
+</a>
+<a href="">
+  <span>
+    游戏
+  
+  </span>
+</a>
+<a href="">
+  <span>
+    科技
+    
+  </span>
+</a>
+<a href="">
+  <span>
+    鬼畜
+  
+  </span>
+</a>
+<a href="">
+  <span>
+    资讯
+   
+  </span>
+</a>
+<a href="">
+  <span>
+    影视
+    
+  </span>
+</a>
+<a href="">
+  <span>
+    更多
+    <i class="fa fa-angle-down" style="font-size: 16px"></i>
+  </span>
+</a>
+    div>
+    </div>
+        </div>
+    </div>
+</div>
+
+    
+
+</template>
+<script>
+import "@/css/daohang.css"
+export default{
+data(){
+    return{
+        
+    }
+},
+methods:{
+   
+}
+}
+</script>
+
